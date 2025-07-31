572115f69c80068c314358f9d518690b90824392
RainhaGomez <gyhsantos2009@gmail.com>
2025-07-31 17:00:12 -0300

    Add files via upload
    Alterações devem ser feitas nos arquivos de senhas em .txt para que o código funcione, principalmente os códigos de MD5

---
+++ quebrarMD5.py
@@ -0,0 +1,27 @@
+#Quebra de hash = hash Cracking
+
+import hashlib
+
+arquivo_hash = input('Digite o nome do arquivo que contém o hash md5: ')
+
+try:
+    with open(arquivo_hash, 'r') as arq:
+        hash_alvo = arq.read().strip()
+except FileExistsError:
+    print('Arquivo não encontrado.')
+    exit()
+
+#Lista de possíveis combinações (dicionário)
+wordlist = ['123', 'admin', 'senha', 'root','teste', 'abc', '123456', 'senha123']
+print(f'Hash Alvo: {hash_alvo}')
+encontrado = False
+
+#Verificação: buscar cada palavra da lista.
+for palavra in wordlist:
+    hash_palavra = hashlib.md5(palavra.encode()).hexdigest()
+    if hash_palavra == hash_alvo:
+        print(f'Senha encontrada: {palavra}')
+        encontrado = True
+        break
+if not encontrado:
+    print('Senha não encontrada na wordlist.')
